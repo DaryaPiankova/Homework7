beaab7857cdac2bdf82a84dac76f2ee52caf627d
Daria <darya.piankova@yandex.ru>
2023-02-20 20:03:42 +0500

    task50

--- Program.cs
+++ Program.cs
@@ -1,14 +1,13 @@
-﻿void InputMatrix(double[,] matrix)
+﻿void InputMatrix(int[,] matrix)
 {
-      double  begin=1, end=100;
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
-            matrix[i, j] = Math.Round(new Random().NextDouble() * (end - begin) + begin, 2);
+            matrix[i, j] = new Random().Next(1, 21); // [1, 20]
     }
 }
 
-void PrintMatrix(double[,] matrix)
+void PrintMatrix(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
@@ -18,10 +17,35 @@ void PrintMatrix(double[,] matrix)
     }
 }
 
+int GetPositionValue(int [] pos,int[,] matrix ){
+    int value=0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++){
+            if(i==pos[0]-1 && j==pos[1]-1){
+                value=matrix[i, j];
+            }
+        }
+    }
+    return value;
+}
+
+
 
 Console.Clear();
 Console.Write("Введите размер массива: ");
 int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
-double[,] matrix = new double[size[0], size[1]];
+int[,] matrix = new int[size[0], size[1]];
 InputMatrix(matrix);
 PrintMatrix(matrix);
+
+Console.Write("Введите позицию элемента массива: ");
+int[] pos = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+
+while(pos[0]>matrix.GetLength(0) ||pos[1]>matrix.GetLength(1) || pos[0]<0 || pos[1]<0 || pos[0] == 0 || pos[1]==0){
+    Console.Write("Такой позиции нет!\nВведите позицию элемента массива: ");
+pos = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+}
+
+int result=GetPositionValue(pos, matrix);
+Console.Write($"На позиции ({pos[0]}, {pos[1]}) находится элемент {result}");
