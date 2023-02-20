743643b2527566211308f7d3599e29f59506eba8
Daria <darya.piankova@yandex.ru>
2023-02-20 20:13:00 +0500

    doptask2

--- Program.cs
+++ Program.cs
@@ -1,13 +1,34 @@
-﻿void InputMatrix(int[,] matrix)
+﻿void InputStartMatrix(char[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
-            matrix[i, j] = new Random().Next(1, 101); // [1, 20]
+            if(i==0 && j>0 && j<3){
+                matrix[i,j]='B';
+            }else if(i==1){
+                matrix[i,j]='B';
+            }else if(i==2 && j>0 && j<3){
+                matrix[i,j]='B';
+            }else{
+                matrix[i,j]='W';
+            }
     }
 }
 
-void PrintMatrix(int[,] matrix)
+
+void InputNegativeMatrix(char[,] matrix)
+{
+    char[] values={'B', 'W'};
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] =values[new Random().Next(0,2)]; 
+    }
+}
+
+
+
+void PrintMatrix(char[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
@@ -17,27 +38,38 @@ void PrintMatrix(int[,] matrix)
     }
 }
 
-void HorisontalTransponation(int[,] matrix){
-    for (int i = 0; i < matrix.GetLength(0)/2; i++)
+int NegativeMatrix(char[,] matrix1, char[,]matrix2){
+int counter=0;
+    for (int i = 0; i < matrix1.GetLength(0); i++)
     {
-        for (int j = 0; j < matrix.GetLength(1); j++){
-            int temp=matrix[i,j];
-            matrix[i,j]=matrix[matrix.GetLength(0)-i-1, j];
-            matrix[matrix.GetLength(0)-i-1, j]=temp;
+        for (int j = 0; j < matrix1.GetLength(1); j++){
+                if(matrix1[i,j]=='B' && matrix2[i,j]=='B'){
+                    counter++;
+                    matrix2[i,j]='W';
+                }else if(matrix1[i,j]=='W' && matrix2[i,j]=='W'){
+                    counter++;
+                    matrix2[i,j]='B';
+                }
         }
 
     }
+    return counter;
 }
-
-
 Console.Clear();
-Console.Write("Введите размер массива: ");
-int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
-int[,] matrix = new int[size[0], size[1]];
-InputMatrix(matrix);
-Console.WriteLine("Начальный массив: ");
-PrintMatrix(matrix);
-HorisontalTransponation(matrix);
-Console.WriteLine("Конечный массив: ");
-PrintMatrix(matrix);
 
+char[,] startMatrix = new char[3, 4];
+char[,] negativeMatrix = new char[3, 4];
+InputStartMatrix(startMatrix);
+
+Console.WriteLine("Обычный рисунок: ");
+PrintMatrix(startMatrix);
+
+Console.WriteLine("Рисунок в негативе, полученный Мишиной программой: ");
+InputNegativeMatrix(negativeMatrix);
+PrintMatrix(negativeMatrix);
+
+Console.Write("Количество ошибок Мишиной программы: ");
+Console.WriteLine(NegativeMatrix(startMatrix, negativeMatrix));
+
+Console.WriteLine("Корректный рисунок в негативе: ");
+PrintMatrix(negativeMatrix);
